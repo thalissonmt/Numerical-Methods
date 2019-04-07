31b6ffedde6ee9885abd10aeb8ba3833857912cf
thalissonmt <tmt2@cin.ufpe.br>
2019-04-06 21:56:49 -0300

    e_inverso e aprimorado

--- metodos.py
+++ metodos.py
@@ -10,11 +10,11 @@ def receberEntrada():
 def interpretarEntrada(linha):
     dado = linha.split()
     if dado[0]=="euler":
-        euler(float(dado[1]),float(dado[2]),float(dado[3]),int(dado[4]),dado[5])
-    # elif dado[0]=="euler_inverso":
-     
-    # elif dado[0]=="euler_aprimorado":
-
+        euler(float(dado[1]),float(dado[2]),float(dado[3]),int(dado[4]),dado[5],true)
+    elif dado[0]=="euler_inverso":
+     	euler_inverso(float(dado[1]),float(dado[2]),float(dado[3]),int(dado[4]),dado[5],true)
+    elif dado[0]=="euler_aprimorado":
+    	euler_aprimorado(float(dado[1]),float(dado[2]),float(dado[3]),int(dado[4]),dado[5],true)
     # elif dado[0]=="runge_kutta":
 
     # elif dado[0]=="adam_bashforth":
@@ -62,17 +62,54 @@ def adicionarSaida(count, yn):
 def adicionarBarraN():
     arqSaida.write('\n')
 
-def euler(y0,t0,h,passos,fn):
-    gerarSaida("Euler", t0, y0, h)
+def euler(y0,t0,h,passos,fn, flag):
     y, t = symbols("y t")
-    fn = lambdify([y, t], fn)
+    func = lambdify([y, t], fn)
     yn = y0
     count = 0
-    adicionarSaida(count, y0)
+    if flag==true:
+    	gerarSaida("Euler", t0, y0, h)
+    	adicionarSaida(count, y0)
     while count <  passos:
-        yn = yn + h*fn(yn, t0 + h*count)
-        adicionarSaida(count+1, yn)
+        yn = yn + h*func(yn, t0 + h*count)
+        if flag==true:
+        	adicionarSaida(count+1, yn)
         count += 1 
+    if flag==true:
+    	adicionarBarraN()
+    return yn
+
+def euler_inverso(y0,t0,h,passos,fn, flag):
+    y, t = symbols("y t")
+    func = lambdify([y, t], fn)
+    yn = y0
+    count = 0
+    if flag==true:
+    	gerarSaida("Euler Inverso", t0, y0, h)
+    	adicionarSaida(count, y0)
+    while count <  passos:
+        yn = yn + h*func(euler(yn, t0 + h*count, h, 1, fn, false), t0 + h*(count+1))
+        if flag==true:
+        	adicionarSaida(count+1, yn)
+        count += 1 
+    if flag==true:
+    	adicionarBarraN()
+
+def euler_aprimorado(y0,t0,h,passos,fn, flag):
+    y, t = symbols("y t")
+    func = lambdify([y, t], fn)
+    yn = y0
+    count = 0
+    if flag==true:
+    	gerarSaida("Euler Aprimorado", t0, y0, h)
+    	adicionarSaida(count, y0)
+    while count <  passos:
+        yn = yn + h/2*(func(euler(yn, t0 + h*count, h, 1, fn, false), t0 + h*(count+1))+func(yn, t0 + h*count))
+        if flag==true:
+        	adicionarSaida(count+1, yn)
+        count += 1 
+    if flag==true:
+    	adicionarBarraN()
 
 arqSaida = open('saida.txt', 'w')
 receberEntrada()
